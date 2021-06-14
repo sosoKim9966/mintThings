2d3f6270e2f5fc3456473732c5ee4554867784a7
77kkyu <77kkyu@gmail.com>
2021-06-14 23:50:12 +0900

    auth mapper어노테이션 추가

--- src/main/java/com/shop/mint/general/auth/mapper/AuthMapper.java
+++ src/main/java/com/shop/mint/general/auth/mapper/AuthMapper.java
@@ -1,7 +1,9 @@
 package com.shop.mint.general.auth.mapper;
 
 import com.shop.mint.general.auth.domain.AuthVO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AuthMapper {
 
     public AuthVO getUserId(String id);

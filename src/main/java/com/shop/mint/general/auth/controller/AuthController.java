2bd69b0cb133f5392323163c0ea547748a72e7eb
77kkyu <77kkyu@gmail.com>
2021-06-22 22:49:08 +0900

    ROLE 변경 JWT토큰 테스트

--- src/main/java/com/shop/mint/general/auth/controller/AuthController.java
+++ src/main/java/com/shop/mint/general/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.shop.mint.general.auth.mapper.AuthMapper;
 import com.shop.mint.general.auth.service.AuthService;
 import com.shop.mint.jwtConfig.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,5 +36,20 @@ public class AuthController {
     public int join(@RequestBody Map<String, String> user) {
         return 0;
     }
+
+    @PostMapping("/test")
+    public String test(@RequestBody Map<String, String> map) { // 토큰을 인자로 받아서 현재 로그인한 userPk 확인
+        System.out.println("1111111 = " + map.get("token"));
+        //jwtTokenProvider.getUserPk(map.get("token"));
+        return jwtTokenProvider.getUserPk(map.get("token"));
+    }
+
+    @PostMapping("/test1")
+    public Authentication test1(@RequestBody Map<String, String> map) { // 토큰을 인자로 받아서 현재 로그인한 userPk 확인
+        System.out.println("1111111 = " + map.get("token"));
+        
+        return jwtTokenProvider.getAuthentication(map.get("token"));
+    }
+
 
 }
--- src/main/java/com/shop/mint/general/auth/service/AuthService.java
+++ src/main/java/com/shop/mint/general/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package com.shop.mint.general.auth.service;
 
 import com.shop.mint.general.auth.domain.AuthVO;
 import com.shop.mint.general.auth.mapper.AuthMapper;
+import com.shop.mint.jwtConfig.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final AuthMapper authMapper;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthVO findByUserId(String userId) {
         return authMapper.findByUserId(userId);
@@ -18,5 +20,6 @@ public class AuthService {
     public int insertJoinUser(AuthVO authVO) {
         return authMapper.insertJoinUser(authVO);
     }
+
 
 }
